c2ddb209ac16c7219d3274f2f039248f966d7bd8
LourenceQ <lawrenceqf@gmail.com>
2023-02-23 22:39:41 -0300

    Implementando testes adicionais

--- src/LoanApplication.Tests.Unit.Controller/HomeControllerTest.cs
+++ src/LoanApplication.Tests.Unit.Controller/HomeControllerTest.cs
@@ -1,22 +1,52 @@
 using LoanApplication.Controllers;
+using LoanApplication.Core.Model;
+using LoanApplication.Core.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 
 namespace LoanApplication.Tests.Unit.Controller;
 
 public class HomeControllerTest
 {
+    private Mock<ILoanRepository> loanRepository;
+    private HomeController homeController;
+
+    public HomeControllerTest()
+    {
+        loanRepository = new Mock<ILoanRepository>();
+        loanRepository.Setup(x => x.GetLoanTypes()).Returns(GetLoanTypes());
+
+        homeController = new HomeController(loanRepository.Object);
+    }
+
     [Fact]
     public void TestIndex()
     {
-        var homeController = new HomeController();
         var result = homeController.Index();
         var viewResult = Assert.IsType<ViewResult>(result);
+        var loanTypes = Assert.IsAssignableFrom<IEnumerable<LoanType>>(viewResult.ViewData["LoanTypes"]);
+        Assert.Equal(2, loanTypes.Count());
+    }
+
+    private List<LoanType> GetLoanTypes()
+    {
+        var loanTypes = new List<LoanType>();
+        loanTypes.Add(new LoanType()
+        {
+            Id = 1,
+            Name = "Car Loan"
+        });
+        loanTypes.Add(new LoanType()
+        {
+            Id = 2,
+            Name = "House Loan"
+        });
+        return loanTypes;
     }
 
     [Fact]
     public void TestAbout()
-    {
-        var homeController = new HomeController();
+    {        
         var result = homeController.About();
         var viewResult = Assert.IsType<ViewResult>(result);
     }
--- src/LoanApplication/Controllers/HomeController.cs
+++ src/LoanApplication/Controllers/HomeController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoanApplication.Core.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LoanApplication.Controllers;
 
 public class HomeController : Controller
 {
-    public HomeController()
-    {}
+    private ILoanRepository loanRepository;
+    public HomeController(ILoanRepository loanRepository)
+    {
+        this.loanRepository = loanRepository;
+    }
 
     public IActionResult Index()
     {
+        var loanTypes = loanRepository.GetLoanTypes();
+        ViewData["LoanTypes"] = loanTypes;
+
         return View();
     }
 
