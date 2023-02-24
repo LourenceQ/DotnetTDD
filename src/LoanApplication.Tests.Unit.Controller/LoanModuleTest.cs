dd6c24ffc67d31299e1eba4d4137354b52ba6c64
LourenceQ <lawrenceqf@gmail.com>
2023-02-24 20:32:45 -0300

    Implementando descarte de setup em construtor

---
+++ src/LoanApplication.Tests.Unit.Controller/LoanModuleTest.cs
@@ -0,0 +1,27 @@
+﻿using LoanApplication.Core;
+using LoanApplication.Core.Model;
+
+namespace LoanApplication.Tests.Unit.Controller;
+
+public class LoanModuleTest : IDisposable
+{
+    public LoanModuleTest(LoanAppContext context)
+    {
+        Context = new LoanAppContext();
+    }
+
+    public LoanAppContext Context { get; private set; }
+    public void Dispose()
+    {
+        Context = null;
+    }
+
+    [Fact]
+    public void TestSAveLoan_ShouldReturnTrue()
+    {
+        Loan loan = new Loan { Description = "Car Loan" };
+        Context.Loan.Add(loan);
+        var isSaved = Context.Save();
+        Assert.True(isSaved);
+    }
+}
