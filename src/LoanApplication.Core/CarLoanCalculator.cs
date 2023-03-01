d8adb4d3f42c7a89ce60fa66f53ec26adafa3c6e
LourenceQ <lawrenceqf@gmail.com>
2023-03-01 20:45:07 -0300

    Implementnado data driven test

---
+++ src/LoanApplication.Core/CarLoanCalculator.cs
@@ -0,0 +1,24 @@
+﻿using LoanApplication.Core.DTO;
+using LoanApplication.Core.Model;
+
+namespace LoanApplication.Core;
+
+public class CarLoanCalculator : LoanCalculator
+{
+    public CarLoanCalculator(IRateParser rateParser) : base(rateParser)
+    {
+        base.rateParser = rateParser;
+    }
+
+    public override Loan CalculateLoan(LoanDTO loanDTO)
+    {
+        Loan loan = new();
+        loan.LoanType = loanDTO.LoanType;
+        loan.InterestRate = rateParser.GetRateByLoanType(loanDTO.LoanType);
+
+        if (loanDTO.LocationTypeDTO == LoanDTO.LocationType.Location2)
+            loan.InterestRate = 10F;
+
+        return loan;
+    }
+}
---
+++ src/LoanApplication.Tests.Unit.Controller/CarLoanCalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using LoanApplication.Core;
+using LoanApplication.Core.DTO;
+using LoanApplication.Core.Model;
+using static LoanApplication.Core.DTO.LoanDTO;
+
+namespace LoanApplication.Tests.Unit.Controller;
+
+public class CarLoanCalculatorTest
+{
+    private CarLoanCalculator carLoanCalculator;
+    public CarLoanCalculator()
+    {
+        RateParser rateParser = new RateParser();
+        this.carLoanCalculator = new CarLoanCalculator(rateParser);
+    }
+
+    [Fact]
+    public void Test_CalculateLoan_ShouldReturnLoan()
+    {
+        // first scenario
+        LoanDTO loanDTO1 = new LoanDTO();
+        loanDTO1.LoanType = LoanType.CarLoan;
+        loanDTO1.LocationType = LocationType.Location1;
+        loanDTO1.JobType = JobType.Professional
+        Loan loan1 = carLoanCalculator.CalculateLoan(loanDTO1);
+
+        Assert.NotNull(loan1);
+        Assert.Equal(8, loan1.InterestRate);
+
+        // second scenario
+        LoanDTO loanDTO2 = new LoanDTO();
+        loanDTO2.LoanType = LoanType.CarLoan;
+        loanDTO2.LocationType = LocationType.Location2;
+        loanDTO2.JobType = JobType.Professional;
+        Loan loan2 = carLoanCalculator.CalculateLoan(loanDTO2);
+
+        Assert.NotNull(loan2);
+        Assert.Equal(10, loan2.InterestRate);
+    }
+}
