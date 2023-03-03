ad2173d2fccdacadae2bf89edb4b78bc5b9be132
LourenceQ <lawrenceqf@gmail.com>
2023-03-02 22:08:32 -0300

    Adicionando inlinData tes

--- src/LoanApplication.Tests.Unit.Controller/CarLoanCalculatorTest.cs
+++ src/LoanApplication.Tests.Unit.Controller/CarLoanCalculatorTest.cs
@@ -8,7 +8,7 @@ namespace LoanApplication.Tests.Unit.Controller;
 public class CarLoanCalculatorTest
 {
     private CarLoanCalculator carLoanCalculator;
-    public CarLoanCalculator()
+    public CarLoanCalculatorTest()
     {
         RateParser rateParser = new RateParser();
         this.carLoanCalculator = new CarLoanCalculator(rateParser);
@@ -19,9 +19,9 @@ public class CarLoanCalculatorTest
     {
         // first scenario
         LoanDTO loanDTO1 = new LoanDTO();
-        loanDTO1.LoanType = LoanType.CarLoan;
-        loanDTO1.LocationType = LocationType.Location1;
-        loanDTO1.JobType = JobType.Professional
+        loanDTO1.LoanTypeDTO = LoanDTO.LoanType.CarLoan;
+        loanDTO1.LocationTypeDTO = LocationType.Location1;
+        loanDTO1.JobTypeDTO = JobType.Professional;
         Loan loan1 = carLoanCalculator.CalculateLoan(loanDTO1);
 
         Assert.NotNull(loan1);
@@ -29,12 +29,28 @@ public class CarLoanCalculatorTest
 
         // second scenario
         LoanDTO loanDTO2 = new LoanDTO();
-        loanDTO2.LoanType = LoanType.CarLoan;
-        loanDTO2.LocationType = LocationType.Location2;
-        loanDTO2.JobType = JobType.Professional;
+        loanDTO2.LoanTypeDTO = LoanDTO.LoanType.CarLoan;
+        loanDTO2.LocationTypeDTO = LocationType.Location2;
+        loanDTO2.JobTypeDTO = JobType.Professional;
         Loan loan2 = carLoanCalculator.CalculateLoan(loanDTO2);
 
         Assert.NotNull(loan2);
         Assert.Equal(10, loan2.InterestRate);
+    }
+
+
+
+    [Theory, InlineData(new LoanDTO
+    {
+        LoanType = LoanDTO.LoanType.CarLoan
+    ,
+        JobType = JobType.Professional,
+        LocationTypeDTO = LocationType.Location1
+    })]
+    public void Test_CalculateLoan_ShouldReturnCorrectRate()
+    {
+        Loan loan = carLoanCalculator.CalculateLoan(loanDTO);
+        Assert.NotNull(loan);
+        Assert.Equal(8, loan.InterestRate);
     }
 }
---
+++ src/LoanApplication.Tests.Unit.Controller/TheoryClass.cs
@@ -0,0 +1,14 @@
+﻿using LoanApplication.Core.DTO;
+
+namespace LoanApplication.Tests.Unit.Controller;
+
+public class TheoryClass
+{
+    [Theory, InlineData(new LoanDTO {LoanType = LoanDTO.LoanType.CarLoan
+        , JobType = LoanDTO.JobType.Professional
+        , LocationType = LoanDTO.LocationType.Location1})]
+    public void Test_CalculateLoan_ShouldReturnBoolean(LoanDTO loanDTO)
+    {
+        Assert.Equal(4, word.Length);
+    }
+}
